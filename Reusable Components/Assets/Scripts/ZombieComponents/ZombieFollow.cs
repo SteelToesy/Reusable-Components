e5248e7276d721cf75e2c1020eb40250a2df5090
Dion Vos <dionvos160@gmail.com>
2023-06-15 11:13:13 +0200

    added some gunsprites

---
+++ Reusable Components/Assets/Scripts/ZombieComponents/ZombieFollow.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZombieFollow : MonoBehaviour
+{
+    [SerializeField] private Transform _target;
+    [SerializeField] private Rigidbody2D _rb;
+
+    [SerializeField] private float _speed;
+    [SerializeField] private float _distanceTilStop;
+
+    private void Awake()
+    {
+        _target = GameObject.Find("Player").transform; // very illegal, must change, pls don't forget. Ugly ass code.
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveToPlayer();
+    }
+
+    void MoveToPlayer()
+    {
+        if (!CloseEnough())
+            _rb.velocity = GetDirection().normalized * _speed;
+        else _rb.velocity = Vector3.zero;
+    }
+
+    public Vector2 GetDirection()
+    {
+        return _target.position - transform.position;
+    }
+
+    public bool CloseEnough()
+    {
+        float distance = Vector3.Distance(transform.position, _target.transform.position);
+        if (distance > _distanceTilStop)
+            return false;
+        else 
+            return true;
+    }
+}

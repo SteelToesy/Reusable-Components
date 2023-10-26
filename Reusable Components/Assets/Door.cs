66d9040c641c17b09e882bb3165fc3e1330a30c7
WJK <wjk@MacBook-Pro-van-WJK.local>
2023-10-26 14:01:00 +0200

    added an example zombie

--- Reusable Components/Assets/Door.cs
+++ Reusable Components/Assets/Door.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Door : MonoBehaviour
 {
     [SerializeField] private GameObject _door;
+
+    [SerializeField] private GameObject _waveManager;
+    [SerializeField] private GameObject _tutorial;
     void Start()
     {
         
@@ -13,7 +16,11 @@ public class Door : MonoBehaviour
     public void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.GetComponent<GunHandler>())
+        {
+            _waveManager.SetActive(true);
+            _tutorial.SetActive(false);
             _door.SetActive(true);
+        }
     }
 
     // Update is called once per frame
